cc16ac6392e34bd1f17cffbdbded992701bf6a94
Lee Stemkoski <stemkoski@gmail.com>
2023-09-25 08:38:46 -0400

    Day 12

---
+++ Day 12 - Keyboard Input/graphics/math/Vector.java
@@ -0,0 +1,107 @@
+package graphics.math;
+import java.util.List;
+import java.awt.Color;
+
+public class Vector
+{
+    public double[] values;
+
+    // initialize zero vector with given size
+    public Vector(int size)
+    {
+        values = new double[size];
+    }
+
+    // initialize with contents
+    public Vector(double... v)
+    {
+        values = new double[v.length];
+        for (int i = 0; i < v.length; i++)
+            values[i] = v[i];
+    }
+
+    public String toString()
+    {
+        String s = "[";
+        for (int i = 0; i < values.length; i++)
+            s += String.format("%6.2f", values[i]);
+        s += "]";
+        return s;
+    }
+
+    // add values (v1, v2, v3, ...) to the components of this vector
+    public void addValues(double... v)
+    {
+        for (int i = 0; i < v.length; i++)
+            values[i] += v[i];
+    }
+
+    // add components of other vector to this vector
+    public void addVector(Vector v)
+    {
+        for (int i = 0; i < v.values.length; i++)
+            values[i] += v.values[i];
+    }
+
+    // multiply components of this vector by a scalar value s
+    public void multiplyScalar(double s)
+    {
+        for (int i = 0; i < values.length; i++)
+            values[i] *= s;
+    }
+
+    // convert a list of vectors to a list of floats (for GPU processing)
+    public static float[] flattenList(List<Vector> vecList)
+    { 
+        int listSize = vecList.size();
+        int vecSize  = vecList.get(0).values.length;
+        float[] flattened = new float[listSize * vecSize];
+        for (int vecNumber = 0; vecNumber < listSize; vecNumber++)
+        {  
+            Vector v = vecList.get(vecNumber);
+            for (int i = 0; i < vecSize; i++)
+                flattened[vecNumber * vecSize + i] = (float)v.values[i];
+        }
+        return flattened;
+    }
+
+    // if this vector contains RGB data, convert it to a vector containing HSV data
+    //   for simpler calculations (like hue adjustment)
+    public Vector toHSV()
+    {
+        int r = (int)(255 * values[0]);
+        int g = (int)(255 * values[1]);
+        int b = (int)(255 * values[2]);
+
+        float[] hsv = new float[3];
+        hsv = Color.RGBtoHSB(r,g,b,hsv);
+        
+        return new Vector( hsv[0], hsv[1], hsv[2] );
+    }
+    
+    // after performing HSV vector calculations, typically need to convert back to an RGB vector for
+    //   use by GPU
+    public Vector toRGB()
+    {
+        float h = (float)values[0];
+        float s = (float)values[1];
+        float v = (float)values[2];
+    
+        int colorInt = Color.HSBtoRGB(h, s, v);
+        Color color = new Color(colorInt);
+        
+        return new Vector( color.getRed() / 255.0, color.getGreen() / 255.0, color.getBlue() / 255.0 );
+        
+    }
+    
+    public static void test()
+    {
+        Vector rgb = new Vector(1,1,0);
+        System.out.println(rgb);
+        Vector hsv = rgb.toHSV();
+        System.out.println(hsv);
+        Vector rgb2 = hsv.toRGB();
+        System.out.println(rgb2);
+        
+    }
+}
---
+++ Day 12 - Keyboard Input/readme.txt
@@ -0,0 +1,5 @@
+New Files for Day 12:
+
+\graphics\core\Input.java
+\graphics\core\Base.java (updated with Input related methods)
+\Test_Input.java
