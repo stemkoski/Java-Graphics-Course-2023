13161ab299356ce8761b17301d6d534a0de80b90
Lee Stemkoski <stemkoski@gmail.com>
2023-10-18 08:52:18 -0400

    update (no RenderSettings)

--- Day 21 - Scene Helpers/graphics/extras/AxesHelper.java
+++ Day 21 - Scene Helpers/graphics/extras/AxesHelper.java
@@ -5,11 +5,10 @@ import graphics.material.LineMaterial;
 
 public class AxesHelper extends Mesh
 {
-	public AxesHelper(float axisLength, int lineWidth)
-	{
-		super( new AxesGeometry(axisLength), 
-			   new LineMaterial() );
-		this.material.uniforms.get("useVertexColors").data = 1;
-		this.material.renderSettings.get("lineWidth").data = lineWidth;
-	}
+    public AxesHelper(float axisLength, int lineWidth)
+    {
+        super( new AxesGeometry(axisLength), 
+               new LineMaterial() );
+        this.material.uniforms.get("useVertexColors").data = 1;
+    }
 }
--- Day 21 - Scene Helpers/graphics/extras/GridHelper.java
+++ Day 21 - Scene Helpers/graphics/extras/GridHelper.java
@@ -6,19 +6,18 @@ import graphics.material.LineMaterial;
 
 public class GridHelper extends Mesh
 {
-	public GridHelper(double size, int divisions, 
-		Vector gridColor, Vector centerColor, int lineWidth )
-	{
-		super( new GridGeometry(size, divisions, gridColor, centerColor), 
-			   new LineMaterial() );
-		this.material.uniforms.get("useVertexColors").data = 1;
-		this.material.renderSettings.get("lineWidth").data = lineWidth;
-	}
+    public GridHelper(double size, int divisions, 
+        Vector gridColor, Vector centerColor, int lineWidth )
+    {
+        super( new GridGeometry(size, divisions, gridColor, centerColor), 
+               new LineMaterial() );
+        this.material.uniforms.get("useVertexColors").data = 1;
+    }
 
-	public GridHelper()
-	{
-		this(10, 10, 
-			new Vector(0.5,0.5,0.5), 
-			new Vector(0.8,0.8,0.8), 1);
-	}
+    public GridHelper()
+    {
+        this(10, 10, 
+            new Vector(0.5,0.5,0.5), 
+            new Vector(0.8,0.8,0.8), 1);
+    }
 }
